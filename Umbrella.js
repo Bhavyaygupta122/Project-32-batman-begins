c353fad846a59c3f10c17ccc314d9c42fe92dfe8
Bhavyaygupta122 <75886491+Bhavyaygupta122@users.noreply.github.com>
2021-04-09 15:44:41 +0530

    Add files via upload

---
+++ Umbrella.js
@@ -0,0 +1,17 @@
+class Umbrella{
+    constructor(x,y){
+        var options ={
+            isStatic:true
+        }
+        this.body=Bodies.circle(x,y,250,options);
+        this.radius=50;  
+        this.Animation=loadAnimation("walking_1.png","walking_2.png","walking_3.png","walking_4.png","walking_5.png","walking_6.png","walking_7.png","walking_8.png",)
+        World.add(world,this.body);
+
+    }
+    display(){
+        var pos=this.body.position;
+        imageMode(CENTER);
+        animation(this.Animation,pos.x,pos.y+30,100,150);
+    }
+}
